07b2735107b4e86880d81e345072096de2607d40
Hossein Borooji Nejad Shirazi <hosseinboroji@gmail.com>
2022-04-06 10:04:48 -0400

    Refactor JavaScript logic & fix the css z-index

--- JavaScript/DropdownEvent.js
+++ JavaScript/DropdownEvent.js
@@ -1,13 +1,5 @@
+import IsVisible from './IsVisible.js';
 const dropdownItems = document.querySelector('#dropdownItems');
 const sidebarMore = document.querySelector('sidebar-more');
 
-sidebarMore.addEventListener('click', () => {
-	const flexString = 'display: flex;';
-	const attr = dropdownItems.getAttribute('style');
-
-	if (attr === flexString) {
-		dropdownItems.style.display = 'none';
-	} else {
-		dropdownItems.style.display = 'flex';
-	}
-});
+IsVisible(sidebarMore, dropdownItems);
